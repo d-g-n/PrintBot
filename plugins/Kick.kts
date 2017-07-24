65eca097092f15464a1b61e6af864fbdf98bff5d
declan <declan.g.neilson@gmail.com>
2017-07-24 13:01:21 +0100

    Added kick and cleaned up some configs

---
+++ plugins/Kick.kts
@@ -0,0 +1,44 @@
+
+
+import com.github.decyg.command.CommandCore
+import com.github.decyg.core.AuditLog
+import com.github.decyg.core.asMessageString
+import com.github.decyg.core.sendConfirmationEmbed
+import com.github.decyg.tokenizer.TextToken
+import com.github.decyg.tokenizer.UserToken
+import sx.blah.discord.handle.obj.Permissions
+
+CommandCore.command {
+    commandAliases = listOf("kick")
+    prettyName = "Kick"
+    description = "Kicks people"
+    requiredPermission = Permissions.KICK
+    argumentParams = listOf(
+            UserToken("a user to kick") isOptional false isGreedy false,
+            TextToken("a reason") isOptional false isGreedy true
+    )
+    behaviour = { event, tokens ->
+
+        val user = tokens[0] as UserToken
+        val reason = tokens.asMessageString(0)
+
+
+        val kickBool = event.channel.sendConfirmationEmbed(
+                event.author,
+                bodyMessage = "Are you really sure you want to kick ${user.mentionedUser?.name ?: ""} (${user.mentionedUser!!.stringID})" +
+                        " for the reason `$reason`"
+        )
+
+        if(kickBool) {
+            event.guild.kickUser(user.mentionedUser, reason)
+
+            AuditLog.log(
+                    event.guild,
+                    "User ${event.author.name} (${event.author.stringID}) has kicked user ${user.mentionedUser?.name ?: ""} (${user.mentionedUser!!.stringID})" +
+                            " for the reason `$reason`"
+            )
+        }
+
+    }
+}
+
--- src/main/kotlin/com/github/decyg/config/ConfigPOKO.kt
+++ src/main/kotlin/com/github/decyg/config/ConfigPOKO.kt
@@ -5,5 +5,5 @@ import com.github.decyg.core.DiscordCore
 data class ConfigPOKO(
         var serverPrefix : String = DiscordCore.configStore[config.defaultprefix],
         var auditLogChannelID : String = "",
-        var pluginSettings : Map<String, String> = emptyMap()
+        var pluginSettings : Map<String, Any> = emptyMap()
 )
